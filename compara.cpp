b934aeb86669326adc8b9136ae21d511da413205
yoli2001 <yolanda.gracia.bautista@utelvt.edu.ec>
2022-09-29 18:23:14 -0500

    examen

---
+++ compara.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+//YOLANDA DEYANEIRA GRACIA BAUTISTA
+int main() 
+{
+	float y,g;
+	cout<<"ponga el primer numero que desea comparar:   ";
+	cin>> y;
+	cout<<"ponga el segundo numero que desesa comparar:  ";
+	cin>> g;
+	if(y>g)
+	{
+		cout<<"primer numero es mayor:  ";
+	}
+	
+	else if(y==g)
+	{
+		cout<<"son iguales:  ";
+	}
+	else if(y<g)
+	{  
+		cout<<" segundo nuemro es mayor:  ";
+	}
+	return(0);
+}
